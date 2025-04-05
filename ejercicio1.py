a410c95230e465fd42650ac440b9a49c189ae6b9
Zerek247 <monster.304@outlook.es>
2025-04-04 18:15:04 -0600

    Agrego ejercios

---
+++ ejercicio1.py
@@ -0,0 +1,26 @@
+print('hola mundo') # Imprime "¡Hola Mundo!" por consola.
+
+#El codigo imprime Hola mundo en la terminal # Escribe un comentario de una sola línea explicando qué hace el código del Ejercicio 1.
+
+print('Arturo', 28 ,"a;os")# Imprime tu nombre y edad en la misma línea utilizando la función print.
+
+print(type("Hola"),type(28),type(1.9))# Usa la función type() para imprimir el tipo de dato de una cadena de texto, un número entero y un número decimal
+"""
+Los tipos 
+de datos son 
+""" # Escribe un comentario en varias líneas explicando qué son los tipos de datos en Python.
+
+print("Hola" + ' Mundo') # Imprime el resultado de concatenar dos cadenas de texto, por ejemplo: "Hola" y "Mundo".
+
+nombre='Arturo'
+edad = 28
+print("Mi nombre es: ", nombre ," y mi edad es:", edad ) # Crea una variable para almacenar tu nombre, otra para tu edad, e imprime ambas en la misma línea.
+
+solicitud = input("Ingresa tu nombre: ") 
+print("Hola " + (solicitud) + "!") # Escribe un programa que solicite al usuario su nombre y lo imprima junto con un saludo.
+
+num1=3
+num2=5.8
+resultado=num1 + num2
+print(f"La suma de {num1} y de {num2} es: {resultado} Y el tipo de dato del resultado es: {type(resultado)}") # Usa print() para mostrar el resultado de la suma de dos números enteros y el tipo de dato resultante.
+# Comenta el código del Ejercicio 9, y explica qué hace cada línea usando comentarios de una sola línea.
